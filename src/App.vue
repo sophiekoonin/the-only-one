2b26afad0f642d48b75159c86edc9a9e7984cb1a
Sophie Koonin <sophie@localghost.dev>
2020-09-26 15:20:54 +0100

    Get props working

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,117 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <p>
-      Client State:
-      {{
-        state == null
-          ? 'null :('
-          : state.status == undefined
-          ? 'Disconnected'
-          : state.status
-      }}
-    </p>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <p>
+            Client State:
+            {{
+                state == null
+                    ? 'null :('
+                    : state.status == undefined
+                    ? 'Disconnected'
+                    : state.status
+            }}
+        </p>
+        <create-game-form
+            v-bind:joinedOrHosting="joinedOrHosting"
+            v-bind:join="join"
+            v-bind:host="host"
+            v-bind:friendlyName="friendlyName"
+            v-bind:uniqueId="uniqueId"
+        />
+    </div>
 </template>
 
 <script>
-import { P2PClient, P2PServer } from './p2p';
-import { PubSubClient, handleMessageFromAbly } from './ably';
-import { Identity } from './identity';
+import { P2PClient, P2PServer } from './p2p'
+import { PubSubClient, handleMessageFromAbly } from './ably'
+import { Identity } from './identity'
+import CreateGameForm from './components/CreateGameForm'
 
 export default {
-  name: 'App',
-  data: () => ({
-    p2pClient: null,
-    p2pServer: null,
-    friendlyName: 'Player-' + crypto.getRandomValues(new Uint32Array(1))[0],
-    uniqueId: 'Session',
-  }),
-  computed: {
-    state: function() {
-      return this.p2pClient != null ? this.p2pClient.state : null;
+    name: 'App',
+    components: {
+        'create-game-form': CreateGameForm
     },
-    transmittedServerState: function() {
-      return this.p2pClient != null ? this.p2pClient.serverState : null;
+    data: () => ({
+        p2pClient: null,
+        p2pServer: null,
+        friendlyName: 'Player-' + crypto.getRandomValues(new Uint32Array(1))[0],
+        uniqueId: 'Session'
+    }),
+    computed: {
+        state: function() {
+            return this.p2pClient != null ? this.p2pClient.state : null
+        },
+        transmittedServerState: function() {
+            return this.p2pClient != null ? this.p2pClient.serverState : null
+        },
+        joinedOrHosting: function() {
+            return this.p2pClient != null || this.p2pServer != null
+        },
+        iAmHost: function() {
+            return this.p2pServer != null
+        }
     },
-    joinedOrHosting: function() {
-      return this.p2pClient != null || this.p2pServer != null;
-    },
-    iAmHost: function() {
-      return this.p2pServer != null;
-    },
-  },
-  methods: {
-    host: async function(evt) {
-      evt.preventDefault();
+    methods: {
+        host: async function(evt) {
+            evt.preventDefault()
 
-      const pubSubClient = new PubSubClient((message, metadata) => {
-        handleMessageFromAbly(
-          message,
-          metadata,
-          this.p2pClient,
-          this.p2pServer
-        );
-      });
+            const pubSubClient = new PubSubClient((message, metadata) => {
+                handleMessageFromAbly(
+                    message,
+                    metadata,
+                    this.p2pClient,
+                    this.p2pServer
+                )
+            })
 
-      const identity = new Identity(this.friendlyName);
-      this.p2pServer = new P2PServer(identity, this.uniqueId, pubSubClient);
-      this.p2pClient = new P2PClient(identity, this.uniqueId, pubSubClient);
+            const identity = new Identity(this.friendlyName)
+            this.p2pServer = new P2PServer(
+                identity,
+                this.uniqueId,
+                pubSubClient
+            )
+            this.p2pClient = new P2PClient(
+                identity,
+                this.uniqueId,
+                pubSubClient
+            )
 
-      await this.p2pServer.connect();
-      await this.p2pClient.connect();
-    },
-    join: async function(evt) {
-      evt.preventDefault();
+            await this.p2pServer.connect()
+            await this.p2pClient.connect()
+        },
+        join: async function(evt) {
+            evt.preventDefault()
 
-      const pubSubClient = new PubSubClient((message, metadata) => {
-        handleMessageFromAbly(
-          message,
-          metadata,
-          this.p2pClient,
-          this.p2pServer
-        );
-      });
+            const pubSubClient = new PubSubClient((message, metadata) => {
+                handleMessageFromAbly(
+                    message,
+                    metadata,
+                    this.p2pClient,
+                    this.p2pServer
+                )
+            })
 
-      const identity = new Identity(this.friendlyName);
-      this.p2pClient = new P2PClient(identity, this.uniqueId, pubSubClient);
+            const identity = new Identity(this.friendlyName)
+            this.p2pClient = new P2PClient(
+                identity,
+                this.uniqueId,
+                pubSubClient
+            )
 
-      await this.p2pClient.connect();
-    },
-  },
-};
+            await this.p2pClient.connect()
+        }
+    }
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/CreateGameForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <form class="form" v-if="!joinedOrHosting">
+        <label for="session-name">Enter a name for your session</label>
+        <input type="text" name="session-name" v-model="uniqueId" />
+        <label for="name-name">Enter your name</label>
+        <input type="text" name="name" v-model="friendlyName" />
+
+        <button v-on:click="host" class="form-button form-button--host">
+            Host a Session
+        </button>
+        <button v-on:click="join" class="form-button">Join a Session</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'CreateGameForm',
+    props: ['joinedOrHosting', 'host', 'join', 'friendlyName', 'uniqueId']
+}
+</script>
