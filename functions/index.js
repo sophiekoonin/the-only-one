e55ec9b220887125d3858819ff4b446cbfcee184
Sophie Koonin <sophie@localghost.dev>
2020-09-26 14:27:15 +0100

    Add pubsub boilerplate

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const Ably = require('ably/promises');
-
-exports.createToken = async function(req, res) {
-  const client = new Ably.Realtime(process.env.ABLY_API_KEY);
-  const tokenRequestData = await client.auth.createTokenRequest({
-    clientId: 'ably-azure-static-site-demo',
-  });
-  res.send(JSON.stringify(tokenRequestData));
-};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,43 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Ably from 'ably';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
+
+class PubSubClient {
+  constructor(onMessageReceivedCallback) {
+    this.connected = false;
+    this.onMessageReceivedCallback = onMessageReceivedCallback;
+  }
+
+  async connect(identity, uniqueId) {
+    if (this.connected) return;
+
+    this.metadata = { uniqueId: uniqueId, ...identity };
+
+    const ably = new Ably.Realtime.Promise({
+      authUrl: '/api/?name=createTokenRequest',
+    });
+    this.channel = await ably.channels.get(`only-one-${uniqueId}`);
+
+    this.channel.subscribe((message) => {
+      this.onMessageReceivedCallback(message.data, this.metadata);
+    });
+
+    this.connected = true;
+  }
+
+  sendMessage(message, targetClientId) {
+    if (!this.connected) {
+      throw 'Client is not connected';
+    }
+
+    message.metadata = this.metadata;
+    message.forClientId = targetClientId ? targetClientId : null;
+    this.channel.publish({ name: 'only-one-message', data: message });
+  }
+}
