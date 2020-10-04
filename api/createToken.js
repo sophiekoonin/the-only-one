f80808bcb1a206ed3570766ba6a80dc094c24824
Sophie Koonin <sophie@localghost.dev>
2020-10-04 15:30:20 +0100

    Fix the clientID

--- api/createToken.js
+++ api/createToken.js
@@ -3,7 +3,7 @@ const Ably = require('ably/promises')
 module.exports = async function(req, res) {
     const client = new Ably.Realtime(process.env.ABLY_API_KEY)
     const tokenRequestData = await client.auth.createTokenRequest({
-        clientId: 'dont-say-the-word'
+        clientId: 'only-one'
     })
     res.json(tokenRequestData).send()
 }
