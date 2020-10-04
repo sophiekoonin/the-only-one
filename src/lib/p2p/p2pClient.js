0dd73f89e0a1dbcbf25666d0b9e3f5850f9c9171
Sophie Koonin <sophie@localghost.dev>
2020-10-04 17:15:35 +0100

    Use Jo's client/server

--- src/lib/p2p/p2pClient.js
+++ src/lib/p2p/p2pClient.js
@@ -1,42 +1,40 @@
+import { Game } from '../game/GameDefinition'
+
 export class P2PClient {
     constructor(identity, uniqueId, ably) {
         this.identity = identity
         this.uniqueId = uniqueId
         this.ably = ably
 
+        this.game = null
         this.serverState = null
+
         this.state = {
             status: 'disconnected',
-            receivedWords: '',
-            gameStarted: false,
-            currentTurn: '',
-            lastMessage: ''
+            instructionHistory: [],
+            lastInstruction: null
         }
     }
+
     async connect() {
         await this.ably.connect(this.identity, this.uniqueId)
-
         this.ably.sendMessage({ kind: 'connected' })
         this.state.status = 'awaiting-acknowledgement'
+        this.depictIt = new Game(this.uniqueId, this.ably)
     }
+
     onReceiveMessage(message) {
         if (message.serverState) {
             this.serverState = message.serverState
         }
 
         switch (message.kind) {
-            case 'connected':
+            case 'connection-acknowledged':
                 this.state.status = 'acknowledged'
                 break
-            case 'word':
-                this.state.receivedWords += ' ' + message.word
-                break
-            case 'turn':
-                this.state.currentTurn = message.turn
-                break
-            case 'game-start':
-                this.state.gameStarted = true
-                this.state.currentTurn = message.turn
+            case 'instruction':
+                this.state.instructionHistory.push(message)
+                this.state.lastInstruction = message
                 break
             default:
                 break
--- src/lib/p2p/p2pServer.js
+++ src/lib/p2p/p2pServer.js
@@ -1,16 +1,17 @@
-import { Game } from '../game/GameDefinition'
+import { Game } from '../game/GameDefinition.js'
 
 export class P2PServer {
     constructor(identity, uniqueId, ably) {
         this.identity = identity
         this.uniqueId = uniqueId
         this.ably = ably
+
         this.stateMachine = Game({
             channel: ably
         })
+
         this.state = {
             players: [],
-            currentTurnIndex: 0,
             hostIdentity: this.identity,
             started: false
         }
@@ -22,46 +23,34 @@ export class P2PServer {
 
     async startGame() {
         this.state.started = true
-        this.ably.sendMessage({
-            kind: 'game-start',
-            turn: this.state.players[0]
-        })
+
+        this.ably.sendMessage({ kind: 'game-start', serverState: this.state })
         this.stateMachine.state.players = this.state.players
         this.stateMachine.run()
     }
 
     async nextRound() {
-        this.stateMachine.startNewRound()
+        this.stateMachine.resetCurrentStepKeepingState()
         this.stateMachine.run()
     }
 
-    // async advanceTurn() {
-    //     const { currentTurnIndex } = this.state
-    //     const newTurnIndex =
-    //         currentTurnIndex < this.state.players.length - 1
-    //             ? currentTurnIndex + 1
-    //             : 0
-    //     this.state.currentTurnIndex = newTurnIndex
-    //     this.ably.sendMessage({
-    //         kind: 'turn',
-    //         turn: this.state.players[newTurnIndex]
-    //     })
-    // }
     onReceiveMessage(message) {
         switch (message.kind) {
             case 'connected':
                 this.onClientConnected(message)
                 break
-            default:
-                break
+            default: {
+                this.stateMachine.handleInput(message)
+            }
         }
     }
+
     onClientConnected(message) {
         this.state.players.push(message.metadata)
         this.ably.sendMessage(
             { kind: 'connection-acknowledged', serverState: this.state },
             message.metadata.clientId
         )
-        this.ably.sendMessage({ kind: 'peer-status', serverState: this.state })
+        this.ably.sendMessage({ kind: 'game-state', serverState: this.state })
     }
 }
