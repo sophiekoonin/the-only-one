7f43bc3e349a6e3ce5158958f13e9bd3413b18ad
Sophie Koonin <sophie@localghost.dev>
2020-10-05 21:23:48 +0100

    add some veeerryy basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="app">
+        <h1>only one</h1>
         <create-game-form
             v-if="!joinedOrHosting"
             :host="host"
@@ -131,13 +132,25 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+body {
+    background: #e2d4ff;
+}
+
 #app {
+    background: #ac9dcc;
+    width: 50vw;
+    min-height: 50vh;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    border-radius: 6px;
+    border: 1px solid #584187;
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
+    color: #241c36;
     margin-top: 60px;
 }
 </style>
